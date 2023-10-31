4c274041d61939353a8640d0f1eda44f516d6c35
dest92 <acebalmatias2019@gmail.com>
2023-10-31 12:36:30 -0300

    basic menu

--- epic-clash/src/app/game/game.ts
+++ epic-clash/src/app/game/game.ts
@@ -1,83 +1,113 @@
-import { Engine, Actor, Color, vec, Input } from "excalibur";
+import { Engine, Actor, Color, vec, Scene, Input } from "excalibur";
 import { Warrior, Mage, Monster, Weapon } from "./characters"; // Asegúrate de que las clases están correctamente importadas
 
-export const initializeGame = (canvasElement: HTMLCanvasElement) => {
-  // Tamaño fijo para el juego
-  const width = 800; // Ancho fijo
-  const height = 400; // Altura fija
+class MainMenuScene extends Scene {
+  onInitialize(engine: Engine) {
+    const playButton = new Actor({
+      pos: vec(engine.halfDrawWidth, engine.halfDrawHeight),
+      width: 100, // Ancho del botón
+      height: 40, // Altura del botón
+      color: Color.Gray, // Color del botón
+    });
 
+    playButton.on("pointerup", () => {
+      engine.goToScene("game");
+    });
+
+    // Habilitar la captura de eventos de puntero para el botón de Play
+    playButton.enableCapturePointer = true;
+
+    this.add(playButton);
+  }
+}
+
+class GameScene extends Scene {
+  onInitialize(engine: Engine) {
+    // Crear un héroe (puede ser Warrior o Mage)
+    const hero = new Actor({
+      pos: vec(100, engine.drawHeight / 2),
+      color: Color.Red,
+      width: 50,
+      height: 50,
+    });
+    const heroCharacter = new Warrior(new Weapon(10)); // Aquí usamos Warrior, pero también podría ser Mage
+
+    // Velocidad a la que se moverá el héroe
+    const speed = 100;
+
+    engine.input.keyboard.on("hold", (evt) => {
+      if (evt.key === Input.Keys.W) {
+        hero.vel = vec(0, -speed);
+      } else if (evt.key === Input.Keys.S) {
+        hero.vel = vec(0, speed);
+      } else if (evt.key === Input.Keys.A) {
+        hero.vel = vec(-speed, 0);
+      } else if (evt.key === Input.Keys.D) {
+        hero.vel = vec(speed, 0);
+      }
+    });
+
+    engine.input.keyboard.on("release", () => {
+      hero.vel = vec(0, 0);
+    });
+
+    // Crear monstruos y sus correspondientes actores
+    const monsters = [
+      new Monster(50, new Weapon(5)),
+      new Monster(50, new Weapon(5)),
+      // ... Añadir más monstruos si es necesario
+    ];
+
+    const monsterActors = monsters.map(
+      (monster) =>
+        new Actor({
+          pos: vec(
+            Math.random() * engine.drawWidth,
+            Math.random() * engine.drawHeight
+          ), // Posición aleatoria dentro del canvas
+          color: Color.Green,
+          width: 50,
+          height: 50,
+        })
+    );
+
+    this.add(hero);
+    monsterActors.forEach((monsterActor) => this.add(monsterActor));
+
+    // Lógica para atacar a un monstruo
+    engine.input.keyboard.on("press", (evt) => {
+      if (evt.key === Input.Keys.C) {
+        console.log("Attacking monsters...");
+        monsterActors.forEach((monsterActor, index) => {
+          if (hero.pos.distance(monsterActor.pos) < 50) {
+            console.log(monsterActor.pos.distance(hero.pos));
+            heroCharacter.attack(monsters[index]); // El héroe ataca al monstruo
+            console.log(`Monster health: ${monsters[index].health}`);
+
+            if (monsters[index].health <= 0) {
+              monsterActor.color = Color.Gray; // Cambiar color si el monstruo muere
+            }
+          }
+        });
+      }
+    });
+  }
+}
+
+export const initializeGame = (canvasElement: HTMLCanvasElement) => {
   const game = new Engine({
     canvasElement: canvasElement,
-    width: width,
-    height: height,
+    width: 800,
+    height: 400,
     backgroundColor: Color.Azure,
   });
 
-  // Crear un héroe (puede ser Warrior o Mage)
-  const hero = new Actor({
-    pos: vec(100, height / 2),
-    color: Color.Red,
-    width: 50,
-    height: 50,
-  });
-  const heroCharacter = new Warrior(new Weapon(10)); // Aquí usamos Warrior, pero también podría ser Mage
+  // Agregar escenas al juego
+  game.add("menu", new MainMenuScene());
+  game.add("game", new GameScene());
 
-  // Velocidad a la que se moverá el héroe
-  const speed = 100;
-
-  game.input.keyboard.on("hold", (evt) => {
-    if (evt.key === Input.Keys.W) {
-      hero.vel = vec(0, -speed);
-    } else if (evt.key === Input.Keys.S) {
-      hero.vel = vec(0, speed);
-    } else if (evt.key === Input.Keys.A) {
-      hero.vel = vec(-speed, 0);
-    } else if (evt.key === Input.Keys.D) {
-      hero.vel = vec(speed, 0);
-    }
-  });
-
-  game.input.keyboard.on("release", () => {
-    hero.vel = vec(0, 0);
-  });
-
-  // Crear monstruos y sus correspondientes actores
-  const monsters = [
-    new Monster(50, new Weapon(5)),
-    new Monster(50, new Weapon(5)),
-    // ... Añadir más monstruos si es necesario
-  ];
-
-  const monsterActors = monsters.map(
-    (monster) =>
-      new Actor({
-        pos: vec(Math.random() * width, Math.random() * height), // Posición aleatoria dentro del canvas
-        color: Color.Green,
-        width: 50,
-        height: 50,
-      })
-  );
-
-  game.add(hero);
-  monsterActors.forEach((monsterActor) => game.add(monsterActor));
-
-  // Lógica para atacar a un monstruo
-  game.input.keyboard.on("press", (evt) => {
-    if (evt.key === Input.Keys.C) {
-      console.log("Attacking monsters...");
-      monsterActors.forEach((monsterActor, index) => {
-        if (hero.pos.distance(monsterActor.pos) < 50) {
-          console.log(monsterActor.pos.distance(hero.pos));
-          heroCharacter.attack(monsters[index]); // El héroe ataca al monstruo
-          console.log(`Monster health: ${monsters[index].health}`);
-
-          if (monsters[index].health <= 0) {
-            monsterActor.color = Color.Gray; // Cambiar color si el monstruo muere
-          }
-        }
-      });
-    }
-  });
+  // Iniciar con el menú principal
+  game.goToScene("menu");
 
   return game;
 };
