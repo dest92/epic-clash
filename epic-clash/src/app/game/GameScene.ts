b255f7ba913799cfd226de73d44c8e477e664a92
dest92 <acebalmatias2019@gmail.com>
2023-11-06 01:41:19 -0300

    pull from dev and some changes

--- epic-clash/src/app/game/GameScene.ts
+++ epic-clash/src/app/game/GameScene.ts
@@ -24,9 +24,71 @@ export class GameScene extends Scene {
   private currentActor: Actor | null = null; // The actor that currently has control
   private currentTurn: boolean = true; // true for heroes' turn, false for monsters' turn
 
-
   onInitialize(engine: Engine) {
-    const characters = createCharacters(2, 3); // 2 héroes y 3 monstruos
+    const characters = createCharacters(1, 3); // 2 héroes y 3 monstruos
+
+    const attackButton = new Actor({
+      pos: vec(engine.halfDrawWidth + 330, engine.halfDrawHeight + 170),
+      width: 130,
+      height: 30,
+      color: Color.Red,
+    });
+
+    attackButton.on("pointerenter", () => {
+      attackButton.color = Color.White;
+    });
+
+    attackButton.on("pointerleave", () => {
+      attackButton.color = Color.Red;
+    });
+
+    attackButton.on("pointerup", () => {
+      this.performHeroAttack();
+    });
+
+    const labelAttack = new Label({
+      font: new Font({
+        family: "PressStart2P",
+        size: 16,
+        unit: FontUnit.Px,
+        textAlign: TextAlign.Center,
+      }),
+      text: "Attack",
+      pos: vec(engine.halfDrawWidth + 330, engine.halfDrawHeight + 180),
+      color: Color.Black,
+    });
+
+    const changeHeroButton = new Actor({
+      pos: vec(engine.halfDrawWidth + 300, engine.halfDrawHeight + 120),
+      width: 180,
+      height: 30,
+      color: Color.Green,
+    });
+
+    changeHeroButton.on("pointerenter", () => {
+      changeHeroButton.color = Color.White;
+    });
+
+    changeHeroButton.on("pointerleave", () => {
+      changeHeroButton.color = Color.Green;
+    });
+
+    changeHeroButton.on("pointerup", () => {
+      this.currentHeroIndex = (this.currentHeroIndex + 1) % this.heroes.length;
+      //deberia cambiar el tipo de heroe y el actor
+    });
+
+    const labelChangeHero = new Label({
+      font: new Font({
+        family: "PressStart2P",
+        size: 16,
+        unit: FontUnit.Px,
+        textAlign: TextAlign.Center,
+      }),
+      text: "Change Hero",
+      pos: vec(engine.halfDrawWidth + 300, engine.halfDrawHeight + 130),
+      color: Color.Black,
+    });
 
     characters.forEach((character) => {
       const actor = new Actor({
@@ -53,51 +115,35 @@ export class GameScene extends Scene {
     });
 
     // Velocidad a la que se moverán los héroes
-    const speed = 40;
-
+    const speed = 100;
 
     engine.input.keyboard.on("press", (evt) => {
       // Lógica para atacar a un monstruo con la tecla "C"
       if (evt.key === Input.Keys.C) {
-
         if (this.currentTurn) {
           this.performHeroAttack();
-         } else {
+        } else {
           this.performMonsterAttack();
-         }
-      } 
-    });
-
-    engine.input.keyboard.on("hold", (evt) => {
-
-        const currentActor = this.currentActor;
-        if (!currentActor) return;
-
-        if (evt.key === Input.Keys.W) {
-          currentActor.vel = vec(0, -speed);
-        } else if (evt.key === Input.Keys.S) {
-          currentActor.vel = vec(0, speed);
-        } else if (evt.key === Input.Keys.A) {
-          currentActor.vel = vec(-speed, 0);
-        } else if (evt.key === Input.Keys.D) {
-          currentActor.vel = vec(speed, 0);
         }
-
+      }
     });
-  }
 
+    this.add(attackButton);
+    this.add(changeHeroButton);
+    this.add(labelAttack);
+    this.add(labelChangeHero);
+  }
 
   update(engine: Engine, delta: number) {
     super.update(engine, delta);
 
-     //If it's the current actor's turn
-    if (this.currentActor) {
-   // Allow the actor to perform an action
-      this.currentActor.rotation = 1
-
-    }
+    // //If it's the current actor's turn
+    // if (this.currentActor) {
+    //   // Allow the actor to perform an action
+    //   this.currentActor.rotation = 1;
+    // }
   }
-     
+
   changeTurn() {
     // Si es el turno de los héroes
     if (this.currentTurn) {
@@ -108,63 +154,63 @@ export class GameScene extends Scene {
     } else {
       // Si es el turno de los monstruos
       // Pasa al siguiente monstruo
-      this.currentMonsterIndex = (this.currentMonsterIndex + 1) % this.monsters.length;
+      this.currentMonsterIndex =
+        (this.currentMonsterIndex + 1) % this.monsters.length;
       // Establece el actor actual como el monstruo actual
       this.currentActor = this.monsters[this.currentMonsterIndex];
     }
-    
+
     // Cambia el turno
     this.currentTurn = !this.currentTurn;
-   }
-   
-   performHeroAttack() {
+  }
+
+  performHeroAttack() {
     const currentHero = this.heroes[this.currentHeroIndex];
     const currentHeroCharacter = this.heroCharacters[this.currentHeroIndex];
     if (!currentHero || !currentHeroCharacter) return;
-   
-    console.log(`->Hero ${this.currentHeroIndex} is attacking...`);
-    
+
+    console.log(`-> ${currentHeroCharacter.name} is attacking...`);
+
     // Genera un índice aleatorio para seleccionar un monstruo al azar
     const randomIndex = Math.floor(Math.random() * this.monsters.length);
-   
+
     const monsterActor = this.monsters[randomIndex];
     const monsterCharacter = this.monsterCharacters[randomIndex];
-   
+
     const damage = currentHeroCharacter.attack(monsterCharacter);
-    console.log(`Monster ${monsterCharacter.name} being attacked`);
+    console.log(`${monsterCharacter.name} being attacked`);
     console.log(`Damage caused: ${damage}`);
     console.log(`Monster health: ${monsterCharacter.health}`);
     if (monsterCharacter.health <= 0) {
       monsterActor.color = Color.Gray;
     }
-   
-    this.changeTurn()
-   }
-   
 
-   performMonsterAttack() {
+    this.changeTurn();
+  }
+
+  performMonsterAttack() {
     console.log("-----Monster turn-----");
-    
-    const currentMonsterCharacter = this.monsterCharacters[this.currentMonsterIndex];
+
+    const currentMonsterCharacter =
+      this.monsterCharacters[this.currentMonsterIndex];
     if (!currentMonsterCharacter) return;
-   
-    console.log(`->Monster ${this.currentMonsterIndex} is attacking...`);
-    
+
+    console.log(`->${currentMonsterCharacter.name} is attacking...`);
+
     // Genera un índice aleatorio para seleccionar un héroe al azar
     const randomIndex = Math.floor(Math.random() * this.heroes.length);
-   
+
     const heroActor = this.heroes[randomIndex];
     const heroCharacter = this.heroCharacters[randomIndex];
-   
+
     const damage = currentMonsterCharacter.attack(heroCharacter);
-    console.log(`Hero ${heroCharacter.name} being attacked`);
+    console.log(`${heroCharacter.name} being attacked`);
     console.log(`Damage caused: ${damage}`);
     console.log(`Hero health: ${heroCharacter.health}`);
     if (heroCharacter.health <= 0) {
-     heroActor.color = Color.Gray;
+      heroActor.color = Color.Gray;
     }
-   
-    this.changeTurn()
-   }
-   
+
+    this.changeTurn();
+  }
 }
